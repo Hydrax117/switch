1a9b269309dd2a788d27da65750982aecddde25f
Hydrax117 <Pauljoseph5000@gmail.com>
2026-09-19 16:01:56 +0100

    feat: add Sentry error monitoring integration
    - Install @sentry/nextjs package
    - Configure withSentryConfig in next.config.ts with source map upload
    - Add instrumentation files for server and client side
    - Add Sentry edge config
    - Add global error boundary (global-error.tsx)
    - Add .env.sentry-build-plugin to .gitignore
    - Include example API route and page for testing Sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://b743f4d808d6343e35c3071f0f5ee716@o4511680827621376.ingest.us.sentry.io/4512112369205248',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+})
